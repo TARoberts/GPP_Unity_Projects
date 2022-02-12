d806de7f29d9f1eebba77a95f973e19ba8ed22b1
Tom <tom3.roberts@live.uwe.ac.uk>
2022-02-12 17:01:12 +0000

    Rework of Movement & rotation code

--- Assets/ThirdPersonMovement.cs
+++ Assets/ThirdPersonMovement.cs
@@ -22,7 +22,6 @@ public class ThirdPersonMovement : MonoBehaviour
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
 
-    float turnSmoothVelocity;
     public float turnSmoothTime = 0.1f;
 
     // Update is called once per frame
@@ -50,12 +49,10 @@ public class ThirdPersonMovement : MonoBehaviour
 
         if (direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            controller.Move(direction * speed * Time.deltaTime);
 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            Quaternion toRotation = Quaternion.LookRotation(direction, Vector3.up); 
+            controller.transform.rotation = Quaternion.RotateTowards(controller.transform.rotation, toRotation, turnSmoothTime * Time.deltaTime);
         }
     }
 }
