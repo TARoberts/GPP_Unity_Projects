b9f4996dc582d9424fdcef5800606665f9b88e93
Tom <tom3.roberts@live.uwe.ac.uk>
2022-02-13 23:16:24 +0000

    Jumping!

--- Assets/ThirdPersonMovement.cs
+++ Assets/ThirdPersonMovement.cs
@@ -17,6 +17,7 @@ public class ThirdPersonMovement : MonoBehaviour
     public float jumpHeight = 3;
     Vector3 velocity;
     bool isGrounded;
+    bool isJumping;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
@@ -24,35 +25,81 @@ public class ThirdPersonMovement : MonoBehaviour
 
     public float turnSmoothTime = 0.1f;
 
+    public Animator animator;
+
     // Update is called once per frame
     void Update()
-    {
+    {  
         //jump
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if (isGrounded && velocity.y < 0)
+        if (!isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
+
+            animator.SetBool("IsJumping", false);
+            animator.SetBool("IsFalling", true);
+            isJumping = false;
+        }
+
+        if (isGrounded)
+        {
+            animator.SetBool("IsJumping", false);
+            animator.SetBool("IsFalling", false);
+            animator.SetBool("IsGrounded", true);
+            isJumping = false;
+        }
+
+        else if (isJumping)
+        {
+            animator.SetBool("IsJumping", false);
+            animator.SetBool("IsFalling", true);
+            animator.SetBool("IsGrounded", false);
+            
         }
 
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+            isJumping = true;
+            animator.SetBool("IsJumping", true);
+            isGrounded = false;
+            animator.SetBool("IsFalling", false);
+            animator.SetBool("IsGrounded", false);
         }
+        else
+        {
+            isJumping = false;
+            animator.SetBool("IsJumping", false);
+        }
+
         //gravity
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
-        //walk
+  
+        //attack
+
+        //move
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        Vector3 direction = new Vector3(horizontal, 0f, vertical);
+        float magnitude = direction.magnitude;
+        direction.Normalize();
 
         if (direction.magnitude >= 0.1f)
         {
-            controller.Move(direction * speed * Time.deltaTime);
-
+            animator.SetFloat("moveSpeed", magnitude);
+            animator.SetBool("Moving", true);
+            
+           //direction = Quaternion.AngleAxis(cam.rotation.eulerAngles.y, Vector3.up) * direction;
+            controller.Move(direction * speed* magnitude * Time.deltaTime);
+            
             Quaternion toRotation = Quaternion.LookRotation(direction, Vector3.up); 
             controller.transform.rotation = Quaternion.RotateTowards(controller.transform.rotation, toRotation, turnSmoothTime * Time.deltaTime);
+        }
+        else
+        {
+            animator.SetBool("Moving", false);
         }
     }
 }
