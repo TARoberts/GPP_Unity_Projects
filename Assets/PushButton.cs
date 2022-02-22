f0190f8e9e7e3564cc79d905d4a0bbd624abf088
Tom Roberts <tom3.roberts@live.uwe.ac.uk>
2022-02-22 12:23:39 +0000

    Button & Door

--- Assets/PushButton.cs
+++ Assets/PushButton.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class PushButton : MonoBehaviour
 {
-    public Animator animator;
+    public Animator animatorButton;
+    public Animator animatorDoor;
+
+    private bool doorOpen = false;
 
     private void OnTriggerEnter(Collider other)
     {
     if (other.tag == "Player")
         {
+            animatorButton.Play("ButtonPush", 0, 0.0f);
+
+            if (!doorOpen)
+            {
+                animatorDoor.Play("Door Slide Down", 0, 0.0f);
+                doorOpen = true;
+            }
             
-            animator.Play("ButtonPush", 0, 0.0f);
         }
     }
 
